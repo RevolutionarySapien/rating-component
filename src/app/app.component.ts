868682d5bd6ef3902b96faff012b509b414fa5e2
neo nthekenyane <neonthekenyane@neos-MacBook-Air.local>
2024-02-08 09:04:58 +0200

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'rating-component';
+  constructor() {}
+  toggle: boolean = true;
+  number: number = 0;
+
+  on1() {
+    this.number = 1;
+    console.log(this.number);
+  }
+
+  on2() {
+    this.number = 2;
+    console.log(this.number);
+  }
+
+  on3() {
+    this.number = 3;
+    console.log(this.number);
+  }
+
+  on4() {
+    this.number = 4;
+    console.log(this.number);
+  }
+
+  on5() {
+    this.number = 5;
+    console.log(this.number);
+  }
+
+  onClick() {
+    this.toggle = false;
+  }
 }
